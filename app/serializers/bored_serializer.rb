7ccce960cb6d56d1223e47e4a863d29faff52312
Ida Olson <idaolson@gmail.com>
2021-11-15 10:04:37 -0700

    Use serializer gem

--- app/serializers/bored_serializer.rb
+++ app/serializers/bored_serializer.rb
@@ -1,32 +1,32 @@
 class BoredSerializer
   include JSONAPI::Serializer
 
-  # set_id { nil }
-  # set_type :activities
-  # attributes :destination, :forecast, :activities
-  def self.activity_serializer(location, forecast, activities)
-    null = nil
-    {
-      "data": {
-        "id": "null",
-        "type": "activities",
-        "attributes": {
-          "destination": location,
-          "forecast": {
-            "summary": ,
-            "temperature":  + '° F'
-          },
-          "activities": {
-            {
-              "type": ,
-              "participants": ,
-              "price":
-            }
-          end
-        }
-      }
-    }
-  end
+  set_id { nil }
+  set_type :activities
+  attributes :destination, :forecast, :activities
+  # def self.activity_serializer(location, forecast, activities)
+  #   null = nil
+  #   {
+  #     "data": {
+  #       "id": "null",
+  #       "type": "activities",
+  #       "attributes": {
+  #         "destination": location,
+  #         "forecast": {
+  #           "summary": ,
+  #           "temperature":  + '° F'
+  #         },
+  #         "activities": {
+  #           {
+  #             "type": ,
+  #             "participants": ,
+  #             "price":
+  #           }
+  #         end
+  #       }
+  #     }
+  #   }
+  # end
 end
 # {
 #   "data": {
