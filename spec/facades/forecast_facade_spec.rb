84bc0d58e1ceb16c201b762ed1a03b03238a35aa
Ida Olson <idaolson@gmail.com>
2021-11-14 20:41:17 -0700

    Rename spec file

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe CityForecastFacade do
+RSpec.describe ForecastFacade do
   it "gets the coordinates of a location" do
-    coords =  CityForecastFacade.city_coords("Denver,CO")
+    coords =  ForecastFacade.city_coords("Denver,CO")
     expect(coords).to be_a(Hash)
     # expect(hazards.count).to eq(4)
     # expect(hazards.first).to be_a(Hazard)
