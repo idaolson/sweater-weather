51c6b24afe40269811d03dd02e6d9e730379a9c4
Ida Olson <idaolson@gmail.com>
2021-11-15 22:44:41 -0700

    Remove commented out code

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -4,4 +4,28 @@ class ActivitySerializer
   set_id { nil }
   set_type :activities
   attributes :destination, :forecast, :activities
+
 end
+# def self.activity_serializer(location, forecast, activities)
+#   null = nil
+#   {
+#     "data": {
+#       "id": "null",
+#       "type": "activities",
+#       "attributes": {
+#         "destination": location,
+#         "forecast": {
+#           "summary": ,
+#           "temperature":  + '° F'
+#         },
+#         "activities": {
+#           {
+#             "type": ,
+#             "participants": ,
+#             "price":
+#           }
+#         end
+#       }
+#     }
+#   }
+# end
