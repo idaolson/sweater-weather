e5e4690881bf187f26fb46aeef65ae98e6b63f16
Ida Olson <idaolson@gmail.com>
2021-11-17 03:57:44 -0700

    Add secure token to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token :api_key
+
   validates :email, :password_digest, presence: true
   validates :email, uniqueness: true
-
-  has_secure_password
-
 end
