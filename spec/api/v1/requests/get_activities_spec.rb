f347ffad5a28c2dfd97b3675bcd9ef5b0814aa9c
Ida Olson <idaolson@gmail.com>
2021-11-16 21:42:56 -0700

    Add tests for all possible happy path results in get activities request

--- spec/api/v1/requests/get_activities_spec.rb
+++ spec/api/v1/requests/get_activities_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Activities Endpoint" do
+RSpec.describe "Activities Endpoint", :vcr do
   before :each do
-    @destination = "chicago,il" # 33 degress here
-    @destination2 = "san antonio,tx" # 79 degress here
+    @destination = "chicago,il" # 51 degress here
+    @destination2 = "san antonio,tx" # 69 degress here
+    @destination3 = "fairbanks,ak" # -1 degress here
   end
 
 
@@ -26,7 +27,7 @@ RSpec.describe "Activities Endpoint" do
       expect(activity.first.to_s).to be_a String
       expect(activity.last).to have_key(:type)
       expect(activity.last[:type]).to_not eq("recreational")
-      expect(activity.last[:type]).to_not eq("busywork")
+      expect(activity.last[:type]).to_not eq("cooking")
       expect(activity.last).to have_key(:participants)
       expect(activity.last).to have_key(:price)
       expect(activity.last).to_not have_key(:accessibility)
@@ -50,6 +51,30 @@ RSpec.describe "Activities Endpoint" do
       expect(activity.first.to_s).to be_a String
       expect(activity.last).to have_key(:type)
       expect(activity.last[:type]).to_not eq("cooking")
+      expect(activity.last[:type]).to_not eq("busywork")
+      expect(activity.last).to have_key(:participants)
+      expect(activity.last).to have_key(:price)
+      expect(activity.last).to_not have_key(:accessibility)
+    end
+
+    get "/api/v1/activities?destination=#{@destination3}"
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(result[:data][:id]).to eq(nil)
+    expect(result[:data][:type]).to eq('activities')
+    expect(result[:data][:attributes][:destination]).to eq(@destination3)
+    expect(result[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(result[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(result[:data][:attributes][:forecast]).to have_key(:temperature)
+    expect(result[:data][:attributes][:forecast]).to_not have_key(:sunrise)
+    expect(result[:data][:attributes][:activities]).to be_a(Hash)
+    expect(result[:data][:attributes][:activities].count).to eq(2)
+    activities = result[:data][:attributes][:activities]
+    activities.each do |activity|
+      expect(activity.first.to_s).to be_a String
+      expect(activity.last).to have_key(:type)
+      expect(activity.last[:type]).to_not eq("recreational")
       expect(activity.last[:type]).to_not eq("busywork")
       expect(activity.last).to have_key(:participants)
       expect(activity.last).to have_key(:price)
