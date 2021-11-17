2b5f0e51fdcee22defe4fac2a8904499cc2dc617
Ida Olson <idaolson@gmail.com>
2021-11-17 01:05:50 -0700

    Add new route to create method in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
       get '/activities', to: 'activities#show'
       get '/backgrounds', to: 'background#show'
+      post '/users', to: 'users#create'
     end
   end
 end
