3ed6da6679894e18d2c56d98060ad2a5d86dbf42
Ida Olson <idaolson@gmail.com>
2021-11-17 03:00:16 -0700

    Add sad paths to user create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,17 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = user_params
     user[:email] = user[:email].downcase
-    new_user = User.create(user)
-    if new_user.save
+    new_user = User.new(user)
+    if params[:email].nil? || params[:password].nil? || params[:password_confirmation].nil?
+      render json: { error: "Missing data." }, status: :bad_request
+    elsif User.find_by(email: new_user.email)
+      render json: { error: "Email in use." }, status: :bad_request
+    elsif params[:password] != params[:password_confirmation]
+      render json: { error: "Passwords do not match." }, status: :bad_request
+    elsif new_user.save
       render json: UserSerializer.new(new_user), status: :created
     else
-      #error message
+      render json: { error: "Invalid request." }, status: :bad_request
     end
   end
 
