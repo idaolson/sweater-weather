00a70c2ad84b70d0cf81a906d6f6a4544a9bb518
Ida Olson <idaolson@gmail.com>
2021-11-16 19:46:59 -0700

    Add route for background image show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/activities', to: 'activities#show'
-
+      get '/backgrounds', to: 'background#show'
     end
   end
 end
