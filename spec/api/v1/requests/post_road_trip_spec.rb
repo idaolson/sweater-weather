2a189394aca3d7f13a723c419975f138034aecf7
Ida Olson <idaolson@gmail.com>
2021-11-17 07:24:13 -0700

    Add additional test to increate simplcov coverage

--- spec/api/v1/requests/post_road_trip_spec.rb
+++ spec/api/v1/requests/post_road_trip_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "RoadTrip endpoint", :vcr do
 
     @start = "Cheyenne,WY"
     @end = "McAllen,TX"
+    @end2 = "Willow,AK"
 
   end
 
@@ -17,6 +18,29 @@ RSpec.describe "RoadTrip endpoint", :vcr do
     body = {
       "start_city": @start,
       "end_city": @end,
+      "api_key": @user.api_key
+    }
+
+    post '/api/v1/road_trip', params: body, as: :json
+
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(result).to be_a(Hash)
+    expect(result[:data]).to be_a(Hash)
+    expect(result[:data][:id]).to eq(nil)
+    expect(result[:data][:type]).to eq('road_trip')
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes].size).to eq(4)
+    expect(result[:data][:attributes]).to have_key(:start_city)
+    expect(result[:data][:attributes]).to have_key(:end_city)
+    expect(result[:data][:attributes]).to have_key(:travel_time)
+    expect(result[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(result[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(result[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+
+    body = {
+      "start_city": @start,
+      "end_city": @end2,
       "api_key": @user.api_key
     }
 
